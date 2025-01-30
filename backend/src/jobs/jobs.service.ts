7ee8166cceab20af753e16dc5d92b2514d77a69c
hamza <hamzahalilovic.12@gmail.com>
2025-01-30 02:57:29 +0100

    Jobs service improved

--- backend/src/jobs/jobs.service.ts
+++ backend/src/jobs/jobs.service.ts
@@ -18,10 +18,10 @@ export class JobsService {
   ) {}
 
   async createJob(totalEmails: number): Promise<Job> {
-    await this.producer.connect();
     const job = this.jobRepo.create({ totalEmails, status: 'pending' });
     const savedJob = await this.jobRepo.save(job);
 
+    await this.producer.connect();
     await this.producer.send({
       topic: 'email-jobs',
       messages: [
@@ -41,9 +41,15 @@ export class JobsService {
   }
 
   async updateJobProgress(id: string, processedEmails: number) {
-    return this.jobRepo.update(id, {
-      processedEmails,
-      status: processedEmails >= 100000 ? 'completed' : 'in-progress',
-    });
+    const job = await this.jobRepo.findOneBy({ id });
+    if (!job) {
+      throw new Error(`Job with ID ${id} not found`);
+    }
+
+    job.processedEmails = processedEmails;
+    job.status =
+      processedEmails >= job.totalEmails ? 'completed' : 'in-progress';
+
+    return this.jobRepo.save(job);
   }
 }
